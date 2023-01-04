9212bad327601f0b03f7f10575bbded567e0f39f
Megan <66484693+megancindric@users.noreply.github.com>
2023-01-04 14:53:45 -0600

    fix: Updated wording of questions, corrected spelling error, shifted Q.6b to it's own bonus problem.

--- app.js
+++ app.js
@@ -121,13 +121,13 @@ console.log("Mexican Foods: ", mexicanFood);
 
 // <<<<<<<<<<<<<<<<< PROBLEMS <<<<<<<<<<<<<<<<<
 
-//1. Create a function that will return all dishes with the cuisine type of "vegetarian"
+//1. Create a function that will return all dishes with the cuisine type of "Vegetarian"
 //Filter
 
 //2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
 
-//3. Create a function that will return only dishes whose serving id number matches their serving count.
+//3. Create a function that will return only dishes whose id number matches their serving count.
 //Filter
 
 //4. Create a function that will return only dishes whose serving count is even.
@@ -136,24 +136,24 @@ console.log("Mexican Foods: ", mexicanFood);
 //5. Create a function that will return dishes whose ingredients array is "tomato" and "cheese".
 //Filter
 
-//6a. Create a function that will return an array of only the names of the cuisine types. Ie ['Italian', 'Mexican', ...]
+//6. Create a function that will return an array of only the names of the cuisine types. Ie ['Italian', 'Mexican', ...]
 //Map
-
-// BONUS: (come back to this after finishing all)
-//6b. Use the filter method to eliminate duplicates, leaving only distinct values in the array
 
 //7. Create a function that will append the cuisine type to the start of the dish's name. Ie, "Italian Pizza"
 //Map
 
-//8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
+//8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. 
+//This function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
 //Map, Filter
 
 // <<<<<<<<<<<<<<<<< BONUS <<<<<<<<<<<<<<<<<
 //9. Create a function that will return dishes whose ingredients array INCLUDES "chickpea".
-//Hint: You do not want to check the array's indexes to find out what the array INCLUDES.
+//Hint: You do not need to check the array's indexes to find out what the array INCLUDES.
 //Filter
 
 //10. Create a function that will return the total serving count of all dishes.
 //Must use Reduce, not a loop.
 
 //11. Create a function that will return an array of any objects that do not share a cuisine type with any other objects.
+
+//12. Revisit your solution for Problem 6.  Use the filter method to eliminate duplicate cuisine types, leaving only distinct values in the array.
